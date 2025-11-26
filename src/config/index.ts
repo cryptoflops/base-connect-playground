1c8c56bddac03430d2b9786b8822c4814bf54236
cryptoflops <cryptoflops@users.noreply.github.com>
2025-11-26 06:28:15 +0200

    fix: use named imports from viem/chains

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 import { cookieStorage, createStorage } from 'wagmi';
-import { base, optimism, arbitrum, celo } from 'wagmi/chains';
+import { base, optimism, arbitrum, celo } from 'viem/chains';
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
@@ -12,7 +12,7 @@ export const networks = [base, optimism, arbitrum, celo] as const;
 
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({
-    storage: cookieStorage,
+    storage: cookieStorage as any,
   }),
   ssr: true,
   networks: [...networks],
